14216d0f4f0d5fd14581ed3d22a8b525beebec05
SagayaJose <sagayajosetj@gmail.com>
2023-09-09 14:04:06 +0530

    Error Changes

--- Mini_Ecommerce_API/Controllers/WeatherForecastController.cs
+++ Mini_Ecommerce_API/Controllers/WeatherForecastController.cs
@@ -25,7 +25,6 @@ namespace Mini_Ecommerce_API.Controllers
 			{
 				Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
 				TemperatureC = Random.Shared.Next(-20, 55),
-				Summary = Summaries[Random.Shared.Next(Summaries.Length)]
 			})
 			.ToArray();
 		}
