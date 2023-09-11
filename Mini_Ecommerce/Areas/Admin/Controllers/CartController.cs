9591af3097f190805e405679f7bd7e5399376c15
SRIDHAR152 <ssri88585@gmail.com>
2023-09-11 04:11:00 -0700

    Template customize

---
+++ Mini_Ecommerce/Areas/Admin/Controllers/CartController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mini_Ecommerce.Framework;
+
+namespace Mini_Ecommerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CartController : Controller
+    {
+
+        public CartController()
+        {
+
+        }
+        [Route(CommonRoutes.Cart)]
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mini_Ecommerce/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mini_Ecommerce.Framework;
+
+namespace Mini_Ecommerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+
+        public CategoriesController()
+        {
+
+        }
+        [Route(CommonRoutes.CategoriesDetails)]
+        public IActionResult CategoriesDetails()
+        {
+            return View();
+        }
+    }
+}
